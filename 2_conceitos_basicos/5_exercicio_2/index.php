fc10a4bf1b35c93aacc0bbe24a8fc9fd39e5575d
henriquejsza <121921605+henriquejsza@users.noreply.github.com>
2023-05-16 20:03:19 -0300

    Add files via upload

---
+++ 2_conceitos_basicos/5_exercicio_2/index.php
@@ -0,0 +1,12 @@
+<?php
+
+  // Esta variável contém o nome da pessoa
+  $nome = "Henrique";
+
+  // Esta variável contém a idade da pessoa
+  $idade = 16;
+
+  // Esta variável contém a profissão da pessoa
+  $profissao = "Programador";
+
+  echo $profissao;
